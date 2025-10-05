6c35d9d83e28788e6d2597740b4a2e412035028b
Keita Hirobe <144866386+hirobekeita@users.noreply.github.com>
2025-10-05 14:37:48 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,8 +9,14 @@ import hiyamugi004 from './ひやむぎ_004_240mm_200g.png';
 import soumen001 from './そうめん_001_189mm_100g.png';
 import yakisabasoumen from './焼きさばそうめん_002_217mm_436g.png';
 import hiyamugi001 from './ひやむぎ_001_206mm_205g.png';
-import soumen010 from './そうめん（乾）_束.png';
-import soumen011 from './そうめん（乾）_1本.png';
+import soumen010 from './そうめん（乾）_022_325mm_400g.png';
+import soumen011 from './手延べそうめん（乾）_002_328mm_300g.png';
+import korokke from './そうめんコロッケ_004_41mm_46g.png';
+import tidimi from './そうめんチヂミ_002_210mm_347g.png';
+import soumen020 from './イベント食_07月_七夕_006_270mm.png';
+import soumen021 from './イベント食_07月_七夕_002_197mm.png';
+import foo001 from './フォー_002_165mm_574g.png';
+import foo002 from './razorramon-hg.png';
 
 // Sample quiz data - can be easily replaced with actual images
 const quizData = [
@@ -37,9 +43,44 @@ const quizData = [
   },
   {
     id: 4,
-    question: "質問4：「より多いそうめん」はどっち？",
+    question: "質問4：「手延べそうめん」はどっち？",
     imageA: soumen010,
     imageB: soumen011,
+    correctAnswer: "B"
+  },
+  {
+    id: 5,
+    question: "質問5：「そうめんコロッケ」はどっち？",
+    imageA: soumen001,
+    imageB: korokke,
+    correctAnswer: "B"
+  },
+  {
+    id: 6,
+    question: "質問6：「そうめんチヂミ」はどっち？",
+    imageA: tidimi,
+    imageB: soumen001,
+    correctAnswer: "A"
+  },
+  {
+    id: 7,
+    question: "質問7：七夕に合っているのはどっち？",
+    imageA: soumen020,
+    imageB: soumen021,
+    correctAnswer: "B"
+  },
+  {
+    id: 8,
+    question: "質問8：「ひやむぎ」はどっち？",
+    imageA: hiyamugi003,
+    imageB: foo001,
+    correctAnswer: "A"
+  },
+  {
+    id: 9,
+    question: "質問9：「フォー！」はどっち？",
+    imageA: foo002,
+    imageB: foo001,
     correctAnswer: "A"
   }
 ];
